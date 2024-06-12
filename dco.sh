4935183d70b200d9d6b56e0f9d92c34412083a1b
Allain Magyar <allain.magyar@iohk.io>
2024-06-12 11:17:56 -0300

    Feat/dco (#2)
    Signed-off-by: Allain Magyar <allain.magyar@iohk.io>

---
+++ dco.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+base_sha=cc6d291d09d466f3b1f7da8c2abf0f76aa85d231
+echo "Base sha $base_sha"
+head_sha=21683236508613777e9b37dcffa67f7240689815
+echo "Head sha $head_sha"
+echo "---"
+echo "Analyzing:"
+dco_check=$(git show --no-patch --format="%B" "$base_sha" | grep 'Signed-off-by:')
+if [ -z "$dco_check" ]; then
+    echo "Commit $base_sha is missing the DCO sign-off!"
+    exit 1
+fi
+
+commits="$(git rev-list --ancestry-path $base_sha..$head_sha)"
+for commit in $commits; do
+dco_check=$(git show --no-patch --format="%B" "$commit" | grep 'Signed-off-by:')
+if [ -z "$dco_check" ]; then
+    echo "Commit $commit is missing the DCO sign-off!"
+    exit 1
+fi
+
+echo "$commit passed"
+done
+echo "---"
+echo "All commits have DCO signed-off"
