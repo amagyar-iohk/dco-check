3b52d8ec3ed0863425fc29e6c5111aa684c1cc31
Allain Magyar <allain.magyar@iohk.io>
2024-06-12 11:08:30 -0300

    feat: final
    Signed-off-by: Allain Magyar <allain.magyar@iohk.io>

--- dco.sh
+++ dco.sh
@@ -2,24 +2,20 @@
 
 base_sha=cc6d291d09d466f3b1f7da8c2abf0f76aa85d231
 echo "Base sha $base_sha"
-head_sha=8eed5feea92e8a9cbb6b5422612f8debcaf2291a
+head_sha=21683236508613777e9b37dcffa67f7240689815
 echo "Head sha $head_sha"
 echo "---"
 commits="$(git rev-list --ancestry-path $base_sha..$head_sha)"
-echo "Commits:"
-echo "$commits"
-echo "---"
+echo "Analyzing:"
 # while [ "$commit_sha" != "$head_sha" ]; do
 for commit in $commits; do
-echo "Analyzing $commit"
-dco_check=$(git show --no-patch --format="%B" "$commit" | grep -q 'Signed-off-by:')
-echo "DCO check $dco_check"
-# if [ "$dco_check" -ne 0 ]; then
-#     echo "Commit $commit_sha is missing the DCO sign-off!"
-#     exit 1
-# fi
+dco_check=$(git show --no-patch --format="%B" "$commit" | grep 'Signed-off-by:')
+if [ -z "$dco_check" ]; then
+    echo "Commit $commit is missing the DCO sign-off!"
+    exit 1
+fi
 
 echo "$commit passed"
 done
-
+echo "---"
 echo "All commits have DCO signed-off"
